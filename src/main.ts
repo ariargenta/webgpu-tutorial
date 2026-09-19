df394af7b89c441c7711dfd019a8d1c23d3a4d1c
ariargenta <55609849+ariargenta@users.noreply.github.com>
2026-09-18 19:41:54 -0600

    feat: create square with gpu buffer

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,25 @@
-import {InitGPU, CreateGPUBuffer} from './helper';
+import {
+    InitGPU
+    , CreateGPUBuffer
+    , CreateTransforms
+    , CreateViewProjection
+} from './helper';
+
 import shader from './shader.wgsl';
 import "./site.css";
+import {CubeData} from './vertex-data';
+import {mat4} from 'gl-matrix';
 
-const CreateSquare = async () => {
+const Create3DObject = async () => {
     const gpu = await InitGPU();
     const device = gpu.device;
-
-    const vertexData = new Float32Array([
-        -0.5, -0.5  // a
-        , 0.5, -0.5 // b
-        , -0.5, 0.5 // d
-        , -0.5, 0.5 // d
-        , 0.5, -0.5 // b
-        , 0.5, 0.5  // c
-    ]);
-
-    const colorData = new Float32Array([
-        1, 0, 0     // a: red
-        , 0, 1, 0   // b: green
-        , 1, 1, 0   // d: yellow
-        , 1, 1, 0   // d: yellow
-        , 0, 1, 0   // b: green
-        , 0, 0, 1   // c: blue
-    ]);
-
-    const vertexBuffer = CreateGPUBuffer(device, vertexData);
-    const colorBuffer = CreateGPUBuffer(device, colorData);
+    const cubeData = CubeData();
+    const numberOfVertices = cubeData.positions.length / 3;
+    const vertexBuffer = CreateGPUBuffer(device, cubeData.positions);
+    const colorBuffer = CreateGPUBuffer(device, cubeData.colors);
 
     const pipeline = device.createRenderPipeline({
-        layout: 'auto'
+        layout: "auto"
         , vertex: {
             module: device.createShaderModule({
                 code: shader
@@ -36,10 +27,10 @@ const CreateSquare = async () => {
             , entryPoint: "vs_main"
             , buffers: [
                 {
-                    arrayStride: 8
+                    arrayStride: 12
                     , attributes: [{
                         shaderLocation: 0
-                        , format: "float32x2"
+                        , format: "float32x3"
                         , offset: 0
                     }]
                 }
@@ -53,7 +44,7 @@ const CreateSquare = async () => {
                 }
             ]
         }
-        , fragment: {
+        , fragment : {
             module: device.createShaderModule({
                 code: shader
             })
@@ -66,33 +57,93 @@ const CreateSquare = async () => {
         }
         , primitive: {
             topology: "triangle-list"
+            , cullMode: 'back'
+        }
+        , depthStencil: {
+            format: "depth24plus"
+            , depthWriteEnabled: true
+            , depthCompare: "less"
         }
     });
 
-    const commandEncoder = device.createCommandEncoder();
+    const modelMatrix = mat4.create();
+    const mvpMatrix = mat4.create();
+    let vpMatrix = mat4.create();
+
+    const vp = CreateViewProjection(
+        gpu.canvas.width / gpu.canvas.height
+    );
+
+    vpMatrix = vp.viewProjectionMatrix;
+
+    const uniformBuffer = device.createBuffer({
+        size: 64
+        , usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+    });
+
+    const uniformBindGroup = device.createBindGroup({
+        layout: pipeline.getBindGroupLayout(0)
+        , entries: [
+            {
+                binding: 0
+                , resource: {
+                    buffer: uniformBuffer
+                    , offset: 0
+                    , size: 64
+                }
+            }
+        ]
+    });
+
     const textureView = gpu.context.getCurrentTexture().createView();
 
-    const renderPass = commandEncoder.beginRenderPass({
+    const depthTexture = device.createTexture({
+        size: [gpu.canvas.width, gpu.canvas.height, 1]
+        , format: "depth24plus"
+        , usage: GPUTextureUsage.RENDER_ATTACHMENT
+    });
+
+    const renderPassDescription = {
         colorAttachments: [{
             view: textureView
             , clearValue: {r: 0.2, g: 0.247, b: 0.314, a: 1.0}
-            //, loadValue: {r: 0.2, g: 0.247, b: 0.314, a: 1.0}
             , loadOp: 'clear'
             , storeOp: 'store'
         }]
-    });
+        , depthStencilAttachment: {
+            view: depthTexture.createView()
+            , deptClearValue: 1.0
+            , depthLoadOp: 'clear'
+            , depthStoreOp: "store"
+            , stencilClearValue: 0
+            //, stencilLoadOp: 'clear'
+            //, stencilStoreOp: "store"
+        }
+    };
+
+    CreateTransforms(modelMatrix);
+
+    mat4.multiply(mvpMatrix, vpMatrix, modelMatrix);
+    device.queue.writeBuffer(uniformBuffer, 0, mvpMatrix as unknown as ArrayBuffer);
+
+    const commandEncoder = device.createCommandEncoder();
+
+    const renderPass = commandEncoder.beginRenderPass(
+        renderPassDescription as GPURenderPassDescriptor
+    );
 
     renderPass.setPipeline(pipeline);
     renderPass.setVertexBuffer(0, vertexBuffer);
     renderPass.setVertexBuffer(1, colorBuffer);
-    renderPass.draw(6);
+    renderPass.setBindGroup(0, uniformBindGroup);
+    renderPass.draw(numberOfVertices);
     renderPass.end();
 
     device.queue.submit([commandEncoder.finish()]);
 }
 
-CreateSquare();
+Create3DObject();
 
 window.addEventListener('resize', function() {
-    CreateSquare();
+    Create3DObject();
 });
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,4 +1,11 @@
 // Vertex shader
+
+struct Uniforms {
+    mvpMatrix : mat4x4<f32>
+}
+
+@binding(0) @group(0) var<uniform> uniforms : Uniforms;
+
 struct Output {
     @builtin(position) Position : vec4<f32>
     , @location(0) vColor : vec4<f32>
@@ -8,7 +15,7 @@ struct Output {
 fn vs_main(@location(0) pos: vec4<f32>, @location(1) color: vec4<f32>) -> Output {
     var output: Output;
 
-    output.Position = pos;
+    output.Position = uniforms.mvpMatrix * pos;
     output.vColor = color;
 
     return output;
